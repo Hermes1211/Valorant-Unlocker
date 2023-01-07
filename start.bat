0beed2206d2a0df17a56be1da96e0822a07f6300
AMNESIAsCHEATS <122159901+AMNESIAsCHEATS@users.noreply.github.com>
2023-01-07 19:47:42 +0100

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,52 @@
+@echo off
+chcp 65001 > nul
+title VALORANT UNLOCKER ^| Amnesia
+mode con: cols=109 lines=26 
+
+echo [38;2;220;95;255m                        ..                        
+echo [38;2;180;95;255m                  .^!7??J????7~:       ..::^^^.        
+echo [38;2;175;95;255m                ^7JYYJJJYY555PP5J~^~7?JYYYPGGGJ       
+echo [38;2;165;95;255m             :?JJJJYY5555555555P55J^:.  ~PG57.       
+echo [38;2;155;95;255m            ^YYYY55555555555555YYJJ^ .~YPJ~.         
+echo [38;2;145;105;255m            :55555555555555JYJJJJJYYJ5Y!:          
+echo [38;2;135;115;255m            ~P5555555[40;37m[4mAmnesia[0m[38;2;160;95;255mJJJYY5PPP?.           
+echo [38;2;125;125;255m           .?P55555YYJJJJJJYY5PPP5YJJ^                
+echo [38;2;115;135;245m        :!Y5JYYYJJJJJJJYY5PP55YJJJYYY.             
+echo [38;2;105;145;235m     .~JPY~. ^JJJJYY55PP5YYJJJYYY555~               
+echo [38;2;95;155;225m   .75GP~  .:^?5PPP55YJJJJYY5555PPY^                 
+echo [38;2;85;165;215m   JGGGPYYYJ?7~^~?JYYYY5555PPPP5J~                  
+echo [38;2;75;175;205m   .^^^::..       .~7JYYYYYYJ7~:                      
+echo [38;2;65;185;195m                       ....                       
+
+
+
+echo [40;37mDetecting Valorant
+
+timeout 2 > nul
+echo [40;32mValorant is detected ![40;37m
+
+timeout 1 > nul
+echo [40;37mSkin injection...[40;37m
+
+timeout 3 > nul
+
+echo [40;37m[1/568] [40;37
+
+timeout 4 > nul
+
+echo [40;37m[243/568] [40;37m
+
+timeout 3 > nul
+
+echo [40;32m[568/568] [40;37m
+
+timeout 1 > nul
+
+
+echo [40;37mSend request to Riot's server[40;37m
+
+py main.py
+
+echo [40;37mFinish[40;37m
+
+pause
